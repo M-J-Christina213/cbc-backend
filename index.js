cab769afec860c72de93cabf438a83e7ccd8a5fc
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-05 18:46:46 +0530

    ♻️ Removed all the product related files to prepare for a new file structure for a product.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-
-import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
@@ -42,7 +40,7 @@ app.use(
   }
 )
 
-app.use("/api/products", productRouter)
+
 app.use("/api/users", userRouter)
 
 app.listen(5000, 
