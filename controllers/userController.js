ced4f99efd6cbbea46bf911075cd53742696ef7d
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-05 16:11:39 +0530

    hiding secret key in user controller file 🔐

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/user.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import dotenv from "dodtenv";
+dotenv.config();
 
 export function createUser(req, res){
 
@@ -40,8 +42,7 @@ export function loginUser(req, res) {
                         isBlocked: user.isBlocked,
                         type: user.type,
                         profilePicture: user.profilePicture,
-                    },
-                    "cbc-secret-key-7973")
+                    }, process.env.SECRET)
 
                 res.json({
                     message: "User logged in",
