3e7dfa11cced9001ef822585d43b51e1b69448b6
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-06 20:01:14 +0530

    ✨ Added route to handle login requests at /api/login

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,6 +6,6 @@ const userRouter = express.Router();
 
 //Below handles the request - /api/users
 userRouter.post('/', createUser)
-
+//Below handles the request - /api/login
 userRouter.post("/login", loginUser)
 export default userRouter;
