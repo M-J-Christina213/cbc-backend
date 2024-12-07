1e09c21e78e3b6bfdbedc1c1d78ecf286e232028
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-07 12:47:54 +0530

    😎 Added createProduct function to allow admins to add new products.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,2 +1,24 @@
 import Product from "../models/product.js";
 
+export function createProduct(req,res){
+    // ! is used to check if isAdmin is false
+    if(!isAdmin(req)){
+        res.json({
+            message : "Please login as adminstrator to add products"
+        })
+    }
+    const newProductData = req.body
+
+    const product = new Product(newProductData)
+
+    product.save().then(()=>{
+      res.json({
+        message: "Product created"
+      })
+    }).catch((error)=>{
+        res.json({
+            message: error
+        })
+    })
+    
+}
