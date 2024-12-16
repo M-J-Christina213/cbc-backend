7f38911ca5e860c9d4ff69dd1723fabff03689df
Merrian Jethuni <m.j.christina321@gmail.com>
2024-12-16 22:40:13 +0530

    🌟 Cleaned up "order before initialization" issue by renaming it to newOrder for clarity 😅

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -29,8 +29,8 @@ export async function createOrder(req, res) {
         newOrderData.orderId = orderId;
         newOrderData.email = req.user.email;
 
-        const order = new order(newOrderData);
-        await order.save()
+        const newOrder = new order(newOrderData);
+        await newOrder.save()
 
         res.json({
             message : "Order created"
