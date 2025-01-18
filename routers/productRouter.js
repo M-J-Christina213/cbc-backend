601478091887bca9511af94802e048454eb9b02f
Merrian Jethuni <m.j.christina321@gmail.com>
2025-01-18 19:43:24 +0530

    🔧 Added route for getProductById in productRouter 🚀

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createProduct, deleteProducts, getProducts, updateProduct } from '../controllers/productController.js';
+import { createProduct, deleteProducts, getProductById, getProducts, updateProduct } from '../controllers/productController.js';
  const productRouter = express.Router();
 
  productRouter.post("/", createProduct)
  productRouter.get("/", getProducts)
+ productRouter.get("/", getProductById)
  productRouter.delete("/:productID", deleteProducts)
  productRouter.put("/:productID", updateProduct)
  export default productRouter;
