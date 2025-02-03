6cdf9b9e33c9ced9a7b5eaa59f12cc41d0d4001c
Merrian Jethuni <m.j.christina321@gmail.com>
2025-02-03 16:40:15 +0530

    🔧 created a route to getQuote and made a sample template with basic requirements to get a quote  ✅

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -100,3 +100,66 @@ export async function getOrders(req,res){
         })
     }
 }
+
+export async function getQuote(req,res){
+    
+
+    try {
+
+        const newOrderData = req.body;
+        const newProductArray = []
+        
+// Loop through orderedItems to find corresponding products and create a new array
+
+        for (let i=0;i<newOrderData.orderedItems.length;i++){
+            const product = await Product.findOne({
+                productId: newOrderData.orderedItems[i].productId
+            })
+
+            if(product==null){
+                res.json({
+                    message : "Product with id " + newOrderData.orderedItems[i].productId + " not found"
+                })
+                return
+            }
+
+            if (product.stock < newOrderData.orderedItems[i].quantity){
+                res.json({
+                    message: "Sorry, not enough stock available for " + product.productName + " Please update your order and try again"
+                })
+            }
+ // Build the product structure for the new order
+            newProductArray[i] = {
+                name : product.productName,
+                price : product.lastPrice,
+                labeledPrice : product.price,
+                discount : product.price - product.lastPrice,
+                quantity : newOrderData.orderedItems[i].quantity,
+                image : product.images[0]
+            }
+
+            //put another for loop before saving the product from each elemnt if quantity can be reduced - Day 10 - 54:35
+        }
+        console.log(newProductArray)
+
+        //Reduce product quanities based on order
+        for (let i=0; i <newOrderData.orderedItems.length;i++){
+            const orderedItem = newOrderData.orderedItems[i];
+
+            await Product.updateOne(
+                { productId : orderedItem.productId },
+                {quantity: Product.stock- orderedItem.quantity }
+            )
+        }
+
+
+         // Assign the new order details including ordered items
+        newOrderData.orderedItems = newProductArray
+
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
